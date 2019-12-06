4f339271f0df604e6b7456672a37fc30b25740e1
Shavlovsii_Ivan <na613c613c@gmail.com>
2019-12-07 02:37:38 +0300

    [FIX] bug with button

--- app/src/main/java/com/example/quiz/GameActivity/PageFragment.java
+++ app/src/main/java/com/example/quiz/GameActivity/PageFragment.java
@@ -21,7 +21,6 @@ import com.example.quiz.Firebase;
 import com.example.quiz.MainActivity.MainActivity;
 import com.example.quiz.R;
 import com.example.quiz.Retrofit.Model.Result;
-import com.example.quiz.Retrofit.Retrofit;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -32,7 +31,6 @@ import java.util.Random;
 public class PageFragment extends Fragment {
 
     private ArrayList<Button> allBtn = new ArrayList<>();
-
     private List<String> answersArr = new ArrayList<>();
 
     private int pageNumber;
@@ -41,10 +39,19 @@ public class PageFragment extends Fragment {
     private static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
 
     private Counter answerCounter = Launcher.answerCounter;
-    private PlayerInformation playerInformation = Launcher.playerInformation;
     private Counter pointCounter = Launcher.pointCounter;
-    private Retrofit retrofit = Launcher.retrofit;
+    private PlayerInformation playerInformation = Launcher.playerInformation;
+    private Result quizInformationForPage;
 
+    private Button btnAnswer0;
+    private Button btnAnswer1;
+    private Button btnAnswer2;
+    private Button btnAnswer3;
+
+    private TextView question;
+    private TextView question_number;
+
+    private FrameLayout background;
 
     public PageFragment() {
     }
@@ -77,43 +84,38 @@ public class PageFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.game_fragment, null);
 
-
-        FrameLayout background = (FrameLayout) view.findViewById(R.id.background);
+        background = (FrameLayout) view.findViewById(R.id.background);
         background.setBackgroundColor(backColor);
 
-        final Button btnAnswer0 = (Button) view.findViewById(R.id.btnAnswer0);
-        final Button btnAnswer1 = (Button) view.findViewById(R.id.btnAnswer1);
-        final Button btnAnswer2 = (Button) view.findViewById(R.id.btnAnswer2);
-        final Button btnAnswer3 = (Button) view.findViewById(R.id.btnAnswer3);
-        final TextView question = (TextView) view.findViewById(R.id.question);
-        final TextView question_number = (TextView) view.findViewById(R.id.question_number);
+        question = (TextView) view.findViewById(R.id.question);
+        question_number = (TextView) view.findViewById(R.id.question_number);
 
-
-        Result quizInformationForPage = QuizListModel.quizList.get(pageNumber);
-
-
-        question_number.setText("Question № " + ++pageNumber);
-        question.setText(quizInformationForPage.getQuestion() + "");
-
-
-        answersArr = quizInformationForPage.getIncorrectAnswers();
-        answersArr.add(quizInformationForPage.getCategory());
-
-        Collections.shuffle(answersArr);
-
-        /** ************ выводим все ответы на кнопки **********  **/
-
-
-        btnAnswer0.setText(answersArr.get(0));
-        btnAnswer1.setText(answersArr.get(1));
-        btnAnswer2.setText(answersArr.get(2));
-        btnAnswer3.setText(answersArr.get(3));
-
+        btnAnswer0 = (Button) view.findViewById(R.id.btnAnswer0);
+        btnAnswer1 = (Button) view.findViewById(R.id.btnAnswer1);
+        btnAnswer2 = (Button) view.findViewById(R.id.btnAnswer2);
+        btnAnswer3 = (Button) view.findViewById(R.id.btnAnswer3);
 
         allBtn.add(btnAnswer0);
         allBtn.add(btnAnswer1);
         allBtn.add(btnAnswer2);
         allBtn.add(btnAnswer3);
+
+        question_number.setText("Question № " + ++pageNumber);
+
+        quizInformationForPage = QuizListModel.quizList.get(pageNumber);
+
+        question.setText(quizInformationForPage.getQuestion() + "");
+
+        answersArr = quizInformationForPage.getIncorrectAnswers();
+        answersArr.add(quizInformationForPage.getCorrectAnswer());
+
+        Collections.shuffle(answersArr);
+
+        /** ************ выводим все ответы на кнопки **********  **/
+
+        for (int j = 0; j < allBtn.size(); j++) {
+            allBtn.get(j).setText(answersArr.get(j));
+        }
 
 
         for (Button i : allBtn) {
@@ -143,7 +145,7 @@ public class PageFragment extends Fragment {
                     int green = Color.rgb(100, 255, 0);
                     int red = Color.rgb(255, 0, 0);
 
-                    if (button.getText().toString().equals(quizInformationForPage.getCorrectAnswer() + "")) { // правильный ответ
+                    if (button.getText().toString().equals(quizInformationForPage.getCorrectAnswer())) { // правильный ответ
                         button.setBackgroundColor(green);
                         /** ****** счётчик ответов правильных ответов величился ****** **/
                         pointCounter.setCounter(pointCounter.getCounter() + 1);
@@ -162,7 +164,7 @@ public class PageFragment extends Fragment {
                         playerInformation.setNumberOfPoints(pointCounter.getCounter());
 
                         /** ****************** добавляем имя, если оно не указанно ******************* **/
-                        if (playerInformation.getUserName().equals(""))
+                        if (playerInformation.getUserName().trim().equals(""))
                             playerInformation.setUserName("NoName");
 
                         Firebase firebase = new Firebase();
